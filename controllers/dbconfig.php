07fd3266631ce89e03f90b96897ecfbbd5b981d8
Shadrack Muwanguzi <shrekdrake@gmail.com>
2019-12-01 13:33:16 +0200

    changes to signup and registerController

--- controllers/dbconfig.php
+++ controllers/dbconfig.php
@@ -9,7 +9,7 @@
 
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
-if (!$db) {
+if (!$db) { 
  die("Connection failed: " . mysqli_connect_error());
 }
 
--- controllers/registerController.php
+++ controllers/registerController.php
@@ -8,13 +8,14 @@ include("dbconfig.php");
 
 // Escape user inputs for security
 $username = mysqli_real_escape_string($db, $_REQUEST['uname']);
-$email = mysqli_real_escape_string($db, $_REQUEST['email']);
 $password = mysqli_real_escape_string($db, $_REQUEST['passw']);
+$email = mysqli_real_escape_string($db, $_REQUEST['email']);
+
  
 // Attempt insert query execution
-$sql = "INSERT INTO users_table (username, passcode, email) VALUES ('$username', '$email', '$password')";
+$sql = "INSERT INTO users_table (username, passcode, email) VALUES ('$username', '$password', '$email')";
 if(mysqli_query($db, $sql)){
-    echo "Records added successfully.";
+    header("Location: ../ui/login.php");
 } else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($db);
 } 
